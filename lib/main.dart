bb4875e361ebd0d505ff8444a414a3ee6a5b2bf7
Angelica <Angelica137@users.noreply.github.com>
2020-08-09 16:15:17 +0100

    add title and background colour

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: Scaffold(appBar: AppBar(title: Text('I am rich! 💰')))),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich! 💰'),
+          backgroundColor: Colors.black12,
+        ),
+      ),
+    ),
   );
 }
